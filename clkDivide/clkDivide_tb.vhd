1c4c0d7ebdbcd5c26d30d5868ff1c3281c644ae8
amodica24 <anthony.modica23@gmail.com>
2019-04-21 16:26:46 -0700

    Update clkDivide_tb.vhd

--- clkDivide/clkDivide_tb.vhd
+++ clkDivide/clkDivide_tb.vhd
@@ -10,7 +10,7 @@ component clkDivide
 port(
 	clk_in   : in std_logic;
 	clk_rst  : in std_logic;
-	clk_out  : out std_logic
+	new_clk  : out std_logic
 	);
 end component;
 
@@ -19,24 +19,25 @@ signal clk_in  : std_logic := '0';
 signal clk_rst : std_logic := '0';
 
 -- outputs
-signal clk_out : std_logic;
+signal new_clk : std_logic;
 
 -- clock period
-constant clk_period: time:= 10ns
+constant clk_period : time := 10 ns; -- insert the clk period being used
 
 begin
     -- Instantiate the Unit Under Test (UUT)
   uut: clkDivide PORT MAP (
     clk_in => clk_in_s,
-    clk_rst => clk_rst_s
+    clk_rst => clk_rst_s,
+    new_clk => new_clk_s
    );
 
 -- clock process
 clk_process : process
    begin
-      clk <= '0';
+      clk_in_s <= '0';
       wait for clk_period/2;
-      clk <= '1';
+      clk_in_s <= '1';
       wait for clk_period/2;
    end process;
 
@@ -44,7 +45,9 @@ clk_process : process
 stim_proc: process
    begin
    wait for 200ns;
-   clk_rst <= '1';
+   clk_rst_s <= '1';
+   wait for 200ns;
+   clk_rst_s <= '0';
 end process
 
 end;
