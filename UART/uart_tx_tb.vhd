f015edef8bdd0946f607df47ce9965373b442786
amodica24 <anthony.modica23@gmail.com>
2019-04-21 17:43:36 -0700

    Update uart_tx_tb.vhd

--- UART/uart_tx_tb.vhd
+++ UART/uart_tx_tb.vhd
@@ -1,3 +1,22 @@
+-----------------------------------------------------------------------------
+-- Project	    :     VHDL MIDI Controller
+-- Author  	    :     Anthony Modica, Blaine Rieger, Brian Palmigiano
+-----------------------------------------------------------------------------
+-- File		    :     uart_tx_tb.vhd
+-- Description  :     This entity is the testbench for the transmitter that 
+--                    sends data from the FPGA's UART port to the computer
+-- 		
+-- Inputs       :     fill in these  - fill in these
+--		        :     fill in these  - fill in these
+--		        :     fill in these  - fill in these
+-- Outputs	    :     fill in these  - fill in these
+--              :     fill in these  - fill in these
+--              :     fill in these  - fill in these
+-----------------------------------------------------------------------------
+-- Version/Notes
+-- 1.0 - 2019-04-29 - Initial Version
+-----------------------------------------------------------------------------
+
 library ieee;
 use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;
@@ -11,9 +30,11 @@ architecture behave of uart_tx is
             clk_cycles_bit : integer := 320
             );
         port (
+            -- inputs
             in_clk       : in  std_logic;
             tx_enable    : in  std_logic;
             tx_byte      : in  std_logic_vector(7 downto 0);
+            -- outputs
             tx_active    : out std_logic;
             tx_serial    : out std_logic;
             tx_done      : out std_logic
@@ -32,6 +53,7 @@ begin
         in_clk     =>   s_in_clk,
         tx_enable  =>   s_tx_enable,
         tx_byte    =>   s_tx_byte,
+        tx_active  =>   s_tx_active,
         tx_serial  =>   s_tx_serial,
         tx_done    =>   s_tx_done
         );
@@ -45,5 +67,4 @@ begin
             
             
             end if;
-
-        
+   end process;
