968ddaf446b48d4b59e2f021b49da66a9416bd11
amodica24 <anthony.modica23@gmail.com>
2019-04-23 17:19:08 -0700

    Update mult16.vhd

--- 16-to-1-bit-Multiplexer/mult16.vhd
+++ 16-to-1-bit-Multiplexer/mult16.vhd
@@ -3,24 +3,24 @@
 -- Author  	    :     Anthony Modica, Blaine Rieger, Brian Palmigiano
 -----------------------------------------------------------------------------
 -- File		    :     mult16.vhd
--- Description  :     This entity is a 16-1 multiplexer
+-- Description      :     This entity is a 16-1 multiplexer
 -- 		
--- Inputs       :     a
---              :     b
---              :     c
---              :     d
---              :     e1
---              :     f
---              :     g
---              :     h
---              :     i
---              :     j
---              :     k
---              :     l
---              :     m
---              :     n
---              :     o
---              :     sel        : 
+-- Inputs           :     a
+--                  :     b
+--                  :     c
+--                  :     d
+--                  :     e1
+--                  :     f
+--                  :     g
+--                  :     h
+--                  :     i
+--                  :     j
+--                  :     k
+--                  :     l
+--                  :     m
+--                  :     n
+--                  :     o
+--                  :     sel     
 
 -- Outputs	    :     new_clk_s - Output clock
 -----------------------------------------------------------------------------
@@ -65,22 +65,21 @@ BEGIN
 	BEGIN 
 		case sel is
 			when "0000" => q <= a;
-            when "0001" => q <= b;
-            when "0010" => q <= c;
-            when "0011" => q <= d;
-            when "0100" => q <= e1;
-            when "0101" => q <= f;
-            when "0110" => q <= g;
-            when "0111" => q <= h;
-            when "1000" => q <= i;
-            when "1001" => q <= j;
-            when "1010" => q <= k;
-            when "1011" => q <= l;
-            when "1100" => q <= m;
-            when "1101" => q <= n;
-            when "1110" => q <= o;
-            when "1111" => q <= p;
-            
-		END IF;
-	END PROCESS mult_process;
-END behav;
+                        when "0001" => q <= b;
+                        when "0010" => q <= c;
+                        when "0011" => q <= d;
+                        when "0100" => q <= e1;
+                        when "0101" => q <= f;
+                        when "0110" => q <= g;
+                        when "0111" => q <= h;
+                        when "1000" => q <= i;
+                        when "1001" => q <= j;
+                        when "1010" => q <= k;
+                        when "1011" => q <= l;
+                        when "1100" => q <= m;
+                        when "1101" => q <= n;
+                        when "1110" => q <= o;
+                        when "1111" => q <= p;
+                end case;
+	end process mult_process;
+end behav;
