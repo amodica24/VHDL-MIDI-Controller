eb110b900bbedb7fbe4ea94df336bfce02919a3c
amodica24 <anthony.modica23@gmail.com>
2019-04-25 19:03:03 -0700

    Update uart_tx.vhd

--- UART/uart_tx.vhd
+++ UART/uart_tx.vhd
@@ -1,21 +1,21 @@
------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
 -- Project     :     VHDL MIDI Controller
 -- Author      :     Anthony Modica, Blaine Rieger, Brian Palmigiano
------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
 -- File        :     uart_tx.vhd
 -- Description :     This entity is the transmitter to send data 
 --                   from the UART port of the FPGA to the computer
 --
--- Inputs      :     fill in these  - fill in these
---             :     fill in these  - fill in these
---             :     fill in these  - fill in these
--- Outputs     :     fill in these  - fill in these
---             :     fill in these  - fill in these
---             :     fill in these  - fill in these
------------------------------------------------------------------------------
+-- Inputs      :     i_Clk          - input clock
+--             :     i_TX_DV        - transmit enable bit
+--             :     i_TX_Byte      - the 8 bits being transmitted
+-- Outputs     :     o_TX_Active    - bit for detecting bits are being transmitted
+--             :     o_TX_Serial    - start bit
+--             :     o_TX_Done      - bit detects when done transmitting
+-----------------------------------------------------------------------------------
 -- Version/Notes
 -- 1.0 - 2019-04-29 - Initial Version
------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------
 
 --UART Transmitter
 
@@ -25,12 +25,14 @@ use ieee.numeric_std.all;
  
 entity UART_TX is
   generic (
-    g_CLKS_PER_BIT : integer := 320     -- (=10Mhz/31250) Needs to be set correctly
+    g_CLKS_PER_BIT : integer := 320     -- = (10Mhz/31250)
     );
   port (
+    -- inputs
     i_Clk       : in  std_logic;
     i_TX_DV     : in  std_logic;
     i_TX_Byte   : in  std_logic_vector(7 downto 0);
+    --outputs
     o_TX_Active : out std_logic;
     o_TX_Serial : out std_logic;
     o_TX_Done   : out std_logic
@@ -58,37 +60,44 @@ begin
          
       case r_SM_Main is
  
-        -- Idle state
+        -- Case 1: Idle state
+        -- active bit input = '0' AND serial bit input = '1'
         when s_Idle =>
-          o_TX_Active <= '0';
-          o_TX_Serial <= '1';         -- '1' = idle
+          o_TX_Active <= '0';        -- '0', idle
+          o_TX_Serial <= '1';        -- '1', idle
           r_TX_Done   <= '0';
           r_Clk_Count <= 0;
           r_Bit_Index <= 0;
- 
+          -- others <= '0'           -- sets the other inputs to '0'
+
+          -- go to next state when enable is 1
           if i_TX_DV = '1' then
             r_TX_Data <= i_TX_Byte;
             r_SM_Main <= s_TX_Start_Bit;
           else
+          -- else, stay in idle
             r_SM_Main <= s_Idle;
           end if;
- 
-           
-        -- Start bit state (start bit = 0)
+        
+        -- Case 2: Start Bit state
+        -- active bit input = '1' and serial bit input = '0'
         when s_TX_Start_Bit =>
-          o_TX_Active <= '1';         -- '1' = active
+          o_TX_Active <= '1';         -- '1', active bit
           o_TX_Serial <= '0';         -- '0' = start bit
- 
-          if r_Clk_Count < g_CLKS_PER_BIT-1 then
+          
+          -- check the end of the clk count to go to next state
+          if (r_Clk_Count < g_CLKS_PER_BIT-1) then
             r_Clk_Count <= r_Clk_Count + 1;
             r_SM_Main   <= s_TX_Start_Bit;
+          
+            -- go to next state (Case 3)
           else
             r_Clk_Count <= 0;
             r_SM_Main   <= s_TX_Data_Bits;
           end if;
  
            
-        -- Data bits state        
+        -- Case 3: Data Bits state
         when s_TX_Data_Bits =>
           o_TX_Serial <= r_TX_Data(r_Bit_Index);   -- data bits
            
@@ -108,27 +117,31 @@ begin
             end if;
           end if;
  
- 
-        -- Stop bit state (stop bit = 1)
+        -- Case 4: Stop bit state
+        -- stop bit = 1
+        -- Stops the transmission of bits
         when s_TX_Stop_Bit =>
-          o_TX_Serial <= '1';           -- '1' = stop bit
+          o_TX_Serial <= '1';           -- '1', stop bit
  
+          -- check the end of the clk count to go to next state
           if r_Clk_Count < g_CLKS_PER_BIT-1 then
             r_Clk_Count <= r_Clk_Count + 1;
             r_SM_Main   <= s_TX_Stop_Bit;
+          
+          -- once reached, go to the next state (Case 5)
           else
-            r_TX_Done   <= '1';         -- '1' = done transmitting all bits
-            r_Clk_Count <= 0;           -- '0' = reset clock count
+            r_TX_Done   <= '1';         -- '1', done transmitting all bits
+            r_Clk_Count <= 0;           -- '0', reset clock count
             r_SM_Main   <= s_Reset;
           end if;
           
-        -- Reset state (1 clock)
+        -- Case 5: Reset state
+        -- active bit = 0 AND done transmitting bit = 1
         when s_Reset =>
           o_TX_Active <= '0';
           r_TX_Done   <= '1';
           r_SM_Main   <= s_Idle;
            
-             
         when others =>
           r_SM_Main <= s_Idle;
  
