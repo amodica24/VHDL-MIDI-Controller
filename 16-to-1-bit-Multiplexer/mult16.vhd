56e2082ed80594b956b8dde9d6ddba5dbd02e68c
amodica24 <anthony.modica23@gmail.com>
2019-04-28 18:15:07 -0700

    Update mult16.vhd

--- 16-to-1-bit-Multiplexer/mult16.vhd
+++ 16-to-1-bit-Multiplexer/mult16.vhd
@@ -19,37 +19,35 @@ Library IEEE;
 Use IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY mult16 is
-	PORT(
-		a   : IN STD_LOGIC_VECTOR(15 downto 0); 
-        sel : in std_logic_vector(3 downto 0);
-		
-		q   : OUT STD_LOGIC
-	);
-
-END mult16;
-ARCHITECTURE rtl OF mult16 IS
-BEGIN
- mult_process : PROCESS (a,sel)
- BEGIN 
-  case sel is
-   when "0000" => q <= a(0);
-   when "0001" => q <= a(1);
-   when "0010" => q <= a(2);
-   when "0011" => q <= a(3);
-   when "0100" => q <= a(4);
-   when "0101" => q <= a(5);
-   when "0110" => q <= a(6);
-   when "0111" => q <= a(7);
-   when "1000" => q <= a(8);
-   when "1001" => q <= a(9);
-   when "1010" => q <= a(10);
-   when "1011" => q <= a(11);
-   when "1100" => q <= a(12);
-   when "1101" => q <= a(13);
-   when "1110" => q <= a(14);
-   when "1111" => q <= a(15);
-
-   when others => q <= 'X';
-   end case;
-   end process mult_process;
-end rtl;
+  PORT(
+    a : IN STD_LOGIC_VECTOR(15 downto 0); 
+    sel : in std_logic_vector(3 downto 0);
+    q   : OUT STD_LOGIC
+  );
+  END mult16;
+  
+  ARCHITECTURE rtl OF mult16 IS
+    BEGIN
+      mult_process : PROCESS (a,sel)
+    BEGIN 
+      case sel is
+       when "0000" => q <= a(0);
+       when "0001" => q <= a(1);
+       when "0010" => q <= a(2);
+       when "0011" => q <= a(3);
+       when "0100" => q <= a(4);
+       when "0101" => q <= a(5);
+       when "0110" => q <= a(6);
+       when "0111" => q <= a(7);
+       when "1000" => q <= a(8);
+       when "1001" => q <= a(9);
+       when "1010" => q <= a(10);
+       when "1011" => q <= a(11);
+       when "1100" => q <= a(12);
+       when "1101" => q <= a(13);
+       when "1110" => q <= a(14);
+       when "1111" => q <= a(15);
+       when others => q <= 'X';
+end case;
+  end process mult_process;
+  end rtl;
