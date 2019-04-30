1c2d80d46452dd2c17b66ad32818704c077b64e2
amodica24 <anthony.modica23@gmail.com>
2019-04-29 20:16:00 -0700

    Update uart_tx.vhd

--- UART/uart_tx.vhd
+++ UART/uart_tx.vhd
@@ -6,12 +6,12 @@
 -- Description :     This entity is the transmitter to send data 
 --                   from the UART port of the FPGA to the computer
 --
--- Inputs      :     clk_in          - input clock
---             :     i_TX_DV        - transmit enable bit
---             :     i_TX_Byte      - the 8 bits being transmitted
--- Outputs     :     o_TX_Active    - bit for detecting bits are being transmitted
---             :     o_TX_Serial    - start bit
---             :     o_TX_Done      - bit detects when done transmitting
+-- Inputs      :     clk_in        - input clock
+--             :     tx_enable     - transmit enable bit
+--             :     tx_data       - the 8 bits being transmitted
+-- Outputs     :     tx_busy       - bit for detecting bits are being transmitted
+--             :     tx_start      - start bit
+--             :     tx_done       - bit detects when done transmitting
 -----------------------------------------------------------------------------------
 -- Version/Notes
 -- 1.0 - 2019-04-29 - Initial Version
@@ -27,30 +27,28 @@ entity uart_tx is
     );
   port (
     -- inputs
-    clk_in       : in  std_logic;
-    i_TX_DV     : in  std_logic;
-    i_TX_Byte   : in  std_logic_vector(7 downto 0);
+    clk_in      : in  std_logic;
+    tx_enable   : in  std_logic;
+    tx_data     : in  std_logic_vector(7 downto 0);
     --outputs
-    o_TX_Active : out std_logic;
-    o_TX_Serial : out std_logic;
-    o_TX_Done   : out std_logic
+    tx_busy     : out std_logic;
+    tx_start    : out std_logic;
+    tx_done     : out std_logic
     );
 end uart_tx;
  
- 
 architecture RTL of uart_tx is
  
-  type t_SM_Main is (idle_state, s_TX_Start_Bit, s_TX_Data_Bits,
-                     s_TX_Stop_Bit, reset_state);
+  type t_SM_Main is (idle_state, find_start_bit, get_data_bits_tx,
+                     find_stop_bit);
   signal r_SM_Main : t_SM_Main := idle_state;
  
-  signal clk_count : integer range 0 to clk_per_bit-1 := 0;
-  signal index : integer range 0 to 7 := 0;  
+  signal clk_count   : integer range 0 to clk_per_bit-1 := 0;
+  signal index       : integer range 0 to 7 := 0;  
   signal r_TX_Data   : std_logic_vector(7 downto 0) := (others => '0');
   signal r_TX_Done   : std_logic := '0';
    
 begin
- 
    
   p_uart_tx : process (clk_in)
   begin
@@ -59,87 +57,79 @@ begin
       case r_SM_Main is
  
         -- Case 1: Idle state
-        -- active bit input = '0' AND serial bit input = '1'
+        -- 'busy' bit input = '0' AND serial bit input = '1'
         when idle_state =>
-          o_TX_Active <= '0';        -- '0', idle
-          o_TX_Serial <= '1';        -- '1', idle
-          r_TX_Done   <= '0';
+          tx_busy   <= '0';    -- '0', idle
+          tx_start  <= '1';    -- '1', idle
+          r_TX_Done <= '0';
           clk_count <= 0;
-          index <= 0;
-          -- others <= '0'           -- sets the other inputs to '0'
-
+          index     <= 0;
+        
           -- go to next state when enable is 1
-          if i_TX_DV = '1' then
-            r_TX_Data <= i_TX_Byte;
-            r_SM_Main <= s_TX_Start_Bit;
+          if (tx_enable = '1') then
+            r_TX_Data <= tx_data;
+            r_SM_Main <= find_start_bit;
           else
           -- else, stay in idle
             r_SM_Main <= idle_state;
           end if;
         
         -- Case 2: Start Bit state
-        -- active bit input = '1' and serial bit input = '0'
-        when s_TX_Start_Bit =>
-          o_TX_Active <= '1';         -- '1', active bit
-          o_TX_Serial <= '0';         -- '0' = start bit
+        -- 'busy' bit input = '1' and serial bit input = '0'
+        when find_start_bit =>
+          tx_busy  <= '1';         -- '1', 'busy' bit
+          tx_start <= '0';         -- '0' = start bit
           
           -- check the end of the clk count to go to next state
           if (clk_count < clk_per_bit-1) then
-            clk_count <= clk_count + 1;
-            r_SM_Main   <= s_TX_Start_Bit;
+            clk_count   <= clk_count + 1;
+            r_SM_Main   <= find_start_bit;
           
             -- go to next state (Case 3)
           else
             clk_count <= 0;
-            r_SM_Main   <= s_TX_Data_Bits;
+            r_SM_Main <= get_data_bits_tx;
           end if;
  
-           
         -- Case 3: Data Bits state
-        when s_TX_Data_Bits =>
-          o_TX_Serial <= r_TX_Data(index);   -- data bits
+        when get_data_bits_tx =>
+          tx_start <= r_TX_Data(index);   -- data bits
            
           if clk_count < clk_per_bit-1 then
-            clk_count <= clk_count + 1;
-            r_SM_Main   <= s_TX_Data_Bits;
+            clk_count   <= clk_count + 1;
+            r_SM_Main   <= get_data_bits_tx;
           else
             clk_count <= 0;
              
             -- Check if we have sent out all bits
             if index < 7 then
               index <= index + 1;
-              r_SM_Main   <= s_TX_Data_Bits;
+              r_SM_Main   <= get_data_bits_tx;
             else
               index <= 0;
-              r_SM_Main   <= s_TX_Stop_Bit;
+              r_SM_Main   <= find_stop_bit;
             end if;
           end if;
  
         -- Case 4: Stop bit state
         -- stop bit = 1
         -- Stops the transmission of bits
-        when s_TX_Stop_Bit =>
-          o_TX_Serial <= '1';           -- '1', stop bit
+        when find_stop_bit =>
+          tx_start <= '1';           -- '1', stop bit
  
           -- check the end of the clk count to go to next state
           if clk_count < clk_per_bit-1 then
             clk_count <= clk_count + 1;
-            r_SM_Main   <= s_TX_Stop_Bit;
+            r_SM_Main   <= find_stop_bit;
           
-          -- once reached, go to the next state (Case 5)
+          -- once reached, go back to idle state
           else
+            tx_busy <= '0';
             r_TX_Done   <= '1';         -- '1', done transmitting all bits
             clk_count <= 0;           -- '0', reset clock count
-            r_SM_Main   <= reset_state;
+            r_SM_Main   <= idle_state;
           end if;
-          
-        -- Case 5: Reset state
-        -- active bit = 0 AND done transmitting bit = 1
-        when reset_state =>
-          o_TX_Active <= '0';
-          r_TX_Done   <= '1';
-          r_SM_Main   <= idle_state;
-           
+
         when others =>
           r_SM_Main <= idle_state;
  
@@ -147,6 +137,6 @@ begin
     end if;
   end process p_uart_tx;
  
-  o_TX_Done <= r_TX_Done;
+  tx_done <= r_TX_Done;
    
 end RTL;
