881c524efdb12b851fca7be5837d3051429edf01
amodica24 <anthony.modica23@gmail.com>
2019-04-29 20:15:14 -0700

    Update uart_tx_tb.vhd

--- UART/uart_tx_tb.vhd
+++ UART/uart_tx_tb.vhd
@@ -5,12 +5,12 @@
 -- File        :     uart_tx_tb.vhd
 -- Description :     This entity is the testbench that is used to test uart_tx
 --
--- Inputs      :     clk_in          - input clock
---             :     i_TX_DV        - transmit enable bit
---             :     i_TX_Byte      - the 8 bits being transmitted
--- Outputs     :     o_TX_Active    - bit for detecting bits are being transmitted
---             :     o_TX_Serial    - start bit
---             :     o_TX_Done      - bit detects when done transmitting
+-- Inputs      :     clk_in       - input clock
+--             :     tx_enable    - transmit enable bit
+--             :     tx_data      - the 8 bits being transmitted
+-- Outputs     :     tx_busy      - bit for detecting bits are being transmitted
+--             :     tx_start     - start bit
+--             :     tx_done      - bit detects when done transmitting
 -----------------------------------------------------------------------------------
 -- Version/Notes
 -- 1.0 - 2019-04-29 - Initial Version
@@ -27,17 +27,17 @@ architecture behave of uart_tx_tb is
  
   component uart_tx is
     generic (
-      g_CLKS_PER_BIT : integer := 115   -- Needs to be set correctly
+      clk_per_bit : integer := 115   -- Needs to be set correctly
       );
     port (
       -- inputs
-      clk_in       : in  std_logic;
-      i_tx_dv     : in  std_logic;
-      i_tx_byte   : in  std_logic_vector(7 downto 0);
+      clk_in     : in  std_logic;
+      tx_enable  : in  std_logic;
+      tx_data    : in  std_logic_vector(7 downto 0);
       -- outputs
-      o_tx_active : out std_logic;
-      o_tx_serial : out std_logic;
-      o_tx_done   : out std_logic
+      tx_busy    : out std_logic;
+      tx_start   : out std_logic;
+      tx_done    : out std_logic
       );
   end component uart_tx;
    
@@ -48,41 +48,41 @@ architecture behave of uart_tx_tb is
  
   constant c_BIT_PERIOD : time := 8680 ns;
    
-  signal r_CLOCK     : std_logic                    := '0';
-  signal r_TX_DV     : std_logic                    := '0';
-  signal r_TX_BYTE   : std_logic_vector(7 downto 0) := (others => '0');
-  signal w_TX_SERIAL : std_logic;
-  signal w_TX_DONE   : std_logic;
+  signal clk_in_s     : std_logic                    := '0';
+  signal tx_enable_s     : std_logic                    := '0';
+  signal tx_data   : std_logic_vector(7 downto 0) := (others => '0');
+  signal tx_start_s : std_logic;
+  signal tx_done_s   : std_logic;
    
 begin
  
   -- Instantiate UART transmitter
   UART_TX_INST : uart_tx
     generic map (
-      g_CLKS_PER_BIT => c_CLKS_PER_BIT
+      clk_per_bit => c_CLKS_PER_BIT
       )
     port map (
-      clk_in       => r_CLOCK,
-      i_tx_dv     => r_TX_DV,
-      i_tx_byte   => r_TX_BYTE,
-      o_tx_active => open,
-      o_tx_serial => w_TX_SERIAL,
-      o_tx_done   => w_TX_DONE
+      clk_in       => clk_in_s,
+      tx_enable     => tx_enable_s,
+      tx_data   => tx_data,
+      tx_busy => open,
+      tx_start => tx_start_s,
+      tx_done   => tx_done_s
       );
 
-  r_CLOCK <= not r_CLOCK after 50 ns;
+  clk_in_s <= not clk_in_s after 50 ns;
    
   process is
   begin
  
     -- Tell the UART to send a command.
-    wait until rising_edge(r_CLOCK);
-    wait until rising_edge(r_CLOCK);
-    r_TX_DV   <= '1';
-    r_TX_BYTE <= X"AB";
-    wait until rising_edge(r_CLOCK);
-    r_TX_DV   <= '0';
-    wait until w_TX_DONE = '1';
+    wait until rising_edge(clk_in_s);
+    wait until rising_edge(clk_in_s);
+    tx_enable_s   <= '1';
+    tx_data <= X"AB";
+    wait until rising_edge(clk_in_s);
+    tx_enable_s   <= '0';
+    wait until tx_done_s = '1';
      
   end process;
    
