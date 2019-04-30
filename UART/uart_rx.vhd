a99a4e06ee8699e4a0f21a593d6dd3c55a10d89a
amodica24 <anthony.modica23@gmail.com>
2019-04-29 19:34:45 -0700

    Update uart_rx.vhd

--- UART/uart_rx.vhd
+++ UART/uart_rx.vhd
@@ -6,10 +6,10 @@
 -- Description :     This entity is the receiver that receives the message 
 --                   being transmitted from the UART port of the FPGA
 --
--- Inputs      :     i_Clk          - clock
---             :     i_RX_Serial    - transmit enable bit
--- Outputs     :     o_RX_DV        - bit for detecting bits are being transmitted
---             :     o_RX_Byte      - start bit
+-- Inputs      :     clk_in          - clock
+--             :     tx_enable    - transmit enable bit
+-- Outputs     :     tx_detect        - bit for detecting bits are being transmitted
+--             :     rx_start      - start bit
 -----------------------------------------------------------------------------------
 -- Version/Notes
 -- 1.0 - 2019-04-29 - Initial Version
@@ -19,130 +19,131 @@ library ieee;
 use ieee.std_logic_1164.ALL;
 use ieee.numeric_std.all;
  
-entity UART_RX is
+entity uart_rx is
   generic (
-    g_CLKS_PER_BIT : integer := 320     -- =(10Mhz/31250)
+    clk_per_bit : integer := 320     -- =(10Mhz/31250)
     );
   port (
     -- inputs
-    i_Clk       : in  std_logic;
-    i_RX_Serial : in  std_logic;
+    clk_in       : in  std_logic;
+    tx_enable : in  std_logic;
     -- outputs
-    o_RX_DV     : out std_logic;
-    o_RX_Byte   : out std_logic_vector(7 downto 0)
+    tx_detect     : out std_logic;
+    rx_start   : out std_logic_vector(7 downto 0)
     );
-end UART_RX;
+end uart_rx;
  
- architecture rtl of UART_RX is
+ architecture rtl of uart_rx is
  
-  type t_SM_Main is (s_Idle, s_RX_Start_Bit, s_RX_Data_Bits,
-                     s_RX_Stop_Bit, s_Reset);
-  signal r_SM_Main : t_SM_Main := s_Idle;
+  type t_SM_Main is (idle_state, get_start_bit, get_bits,
+                     get_stop_bit, reset_state);
+  signal r_SM_Main : t_SM_Main := idle_state;
  
   signal r_RX_Data_R : std_logic := '0';
   signal r_RX_Data   : std_logic := '0';
 
-  signal r_Clk_Count : integer range 0 to g_CLKS_PER_BIT-1 := 0;
-  signal r_Bit_Index : integer range 0 to 7 := 0;  
+  signal clk_count : integer range 0 to clk_per_bit-1 := 0;
+  signal index : integer range 0 to 7 := 0;  
   signal r_RX_Byte   : std_logic_vector(7 downto 0) := (others => '0');
   signal r_RX_DV     : std_logic := '0';
    
 begin
  
-  p_SAMPLE : process (i_Clk)
+  p_clk : process (clk_in)
   begin
-    if rising_edge(i_Clk) then
-      r_RX_Data_R <= i_RX_Serial;
+    if rising_edge(clk_in) then
+      r_RX_Data_R <= tx_enable;
       r_RX_Data   <= r_RX_Data_R;
     end if;
-  end process p_SAMPLE;
+  end process p_clk;
    
-  p_UART_RX : process (i_Clk)
+  p_uart_rx : process (clk_in)
   begin
-    if rising_edge(i_Clk) then
+    if rising_edge(clk_in) then
       case r_SM_Main is
  
         -- Case 1: Idle state
-        when s_Idle =>
+        when idle_state =>
           r_RX_DV     <= '0';
-          r_Clk_Count <= 0;
-          r_Bit_Index <= 0;
+          clk_count <= 0;
+          index <= 0;
           
           -- check when start bit is low
-          if r_RX_Data = '0' then         -- start bit = '0'
-            r_SM_Main <= s_RX_Start_Bit;
+          if r_RX_Data = '0' then       -- start bit = '0'
+            r_SM_Main <= get_start_bit;
           else
-            r_SM_Main <= s_Idle;
+            r_SM_Main <= idle_state;
           end if;
            
         -- Case 2: Start bit state 
-        when s_RX_Start_Bit =>
+        -- checks for the start bit
+        when get_start_bit =>
         -- checks if middle of start bit is low
-          if r_Clk_Count = (g_CLKS_PER_BIT-1)/2 then
+          if clk_count = (clk_per_bit-1)/2 then
             -- then check if the start bit is low
             if r_RX_Data = '0' then
               -- reset the counter and go to the next state
-              r_Clk_Count <= 0;
-              r_SM_Main   <= s_RX_Data_Bits;
+              clk_count <= 0;
+              r_SM_Main   <= get_bits;
             -- go to idle state
             else
-              r_SM_Main   <= s_Idle;
+              r_SM_Main   <= idle_state;
             end if;
           -- if middle state is not low, loop back to start bit state
           else
-            r_Clk_Count <= r_Clk_Count + 1;
-            r_SM_Main   <= s_RX_Start_Bit;
+            clk_count <= clk_count + 1;
+            r_SM_Main   <= get_start_bit;
           end if;
  
-           
-        -- Case 3: Data bits state
-        when s_RX_Data_Bits =>
-          if r_Clk_Count < g_CLKS_PER_BIT-1 then
-            r_Clk_Count <= r_Clk_Count + 1;
-            r_SM_Main   <= s_RX_Data_Bits;
+        -- Case 3: Begin receiving all 8 bits
+        when get_bits =>
+          if clk_count < clk_per_bit-1 then
+            clk_count <= clk_count + 1;
+            r_SM_Main   <= get_bits;
           else
-            r_Clk_Count            <= 0;
-            r_RX_Byte(r_Bit_Index) <= r_RX_Data;
+            clk_count            <= 0;
+            r_RX_Byte(index) <= r_RX_Data;
              
             -- Check if we have received all bits
-            if r_Bit_Index < 7 then
-              r_Bit_Index <= r_Bit_Index + 1;
-              r_SM_Main   <= s_RX_Data_Bits;
+            if index < 7 then
+              index <= index + 1;
+              r_SM_Main   <= get_bits;
             -- go to the next state to check for the stop bit
             else
-              r_Bit_Index <= 0;
-              r_SM_Main   <= s_RX_Stop_Bit;
+              index <= 0;
+              r_SM_Main   <= get_stop_bit;
             end if;
           end if;
  
         -- Case 4: Stop bit state
         -- check when stop bit is high
-        when s_RX_Stop_Bit =>
-          if r_Clk_Count < g_CLKS_PER_BIT-1 then
-            r_Clk_Count <= r_Clk_Count + 1;
-            r_SM_Main   <= s_RX_Stop_Bit;
+        when get_stop_bit =>
+          if clk_count < clk_per_bit-1 then
+            clk_count <= clk_count + 1;
+            r_SM_Main   <= get_stop_bit;
+
           -- reset the count and stop bit goes high
           -- go to the next state 
           else
             r_RX_DV     <= '1';    -- stop bit
-            r_Clk_Count <= 0;
-            r_SM_Main   <= s_Reset;
+            clk_count <= 0;
+            r_SM_Main   <= reset_state;
           end if;
  
         -- Case 5: reset state
         -- resets for one clock cycle
-        when s_Reset =>
-          r_SM_Main <= s_Idle;
+        when reset_state =>
+          r_SM_Main <= idle_state;
           r_RX_DV   <= '0';
  
         when others =>
-          r_SM_Main <= s_Idle;
+          r_SM_Main <= idle_state;
  
       end case;
     end if;
-  end process p_UART_RX;
+  end process p_uart_rx;
  
-  o_RX_DV   <= r_RX_DV;
-  o_RX_Byte <= r_RX_Byte;
+  tx_detect <= r_RX_DV;
+  rx_start  <= r_RX_Byte;
    
 end rtl;
