0077b43bc42960643238fc7408fe54ce14ebf2d7
amodica24 <anthony.modica23@gmail.com>
2019-05-05 15:29:42 -0700

    Update clkDivide_tb.vhd

--- clkDivide/clkDivide_tb.vhd
+++ clkDivide/clkDivide_tb.vhd
@@ -1,14 +1,14 @@
 -----------------------------------------------------------------------------
--- Project      :     VHDL MIDI Controller
--- Author       :     Anthony Modica, Blaine Rieger, Brian Palmigiano
+-- Project	:     VHDL MIDI Controller
+-- Author  	:     Anthony Modica, Blaine Rieger, Brian Palmigiano
 -----------------------------------------------------------------------------
--- File         :     clkDivide_tb.vhd
+-- File		:     clkDivide_tb.vhd
 -- Description  :     This entity is a clock divider to be implemented
---                    with the analog-to-digital converter and multiplexer
---
--- Inputs       :     clk_100MHz  - 100 MHz input clock
---              :     clk_rst_s   - Reset for the clock
--- Outputs      :     clk_1MHz    - 1 MHz output clock
+--             	      with the analog-to-digital converter and multiplexer
+-- 		
+-- Inputs       :     clk_in_s  - Input for the clock
+--		:     clk_rst_s - Reset for the clock
+-- Outputs	:     new_clk_s - Output clock
 -----------------------------------------------------------------------------
 -- Version/Notes
 -- 1.0 - 2019-04-29 - Initial Version
@@ -24,17 +24,17 @@ architecture rtl of clkDivide_tb is
 
 component clkDivide
 port(
-	clk_100MHz   : in std_logic;
-	clk_rst      : in std_logic;
-	clk_1MHz     : out std_logic
+	clk_in   : in std_logic;
+	clk_rst  : in std_logic;
+	new_clk  : out std_logic
 	);
 end component;
 
 -- inputs
-signal clk_100MHz_s   : std_logic;
-signal clk_rst_s      : std_logic;
+signal clk_in_s   : std_logic;
+signal clk_rst_s  : std_logic;
 -- outputs
-signal clk_1MHz_s     : std_logic;
+signal new_clk_s :  std_logic;
 
 -- clock period
 constant clk_period : time := 10 ns; -- insert the clk period being used
@@ -42,28 +42,28 @@ constant clk_period : time := 10 ns; -- insert the clk period being used
 begin
     -- Instantiate the Unit Under Test (UUT)
   uut: clkDivide PORT MAP (
-    clk_100MHz => clk_100MHz_s,
-    clk_rst    => clk_rst_s,
-    clk_1MHz   => clk_1MHz_s
+    clk_in => clk_in_s,
+    clk_rst => clk_rst_s,
+    new_clk => new_clk_s
    );
 
 -- clock process
 clk_process : process
    begin
-    clk_100MHz_s <= '0';
-    wait for clk_period/2;
-    clk_100MHz_s <= '1';
-    wait for clk_period/2;
+      clk_in_s <= '0';
+      wait for clk_period/2;
+      clk_in_s <= '1';
+      wait for clk_period/2;
    end process;
 
 -- stimulus process
 -- test each case for the reset
 stim_proc: process
-  begin
-  clk_rst_s <= '1'; 
-  wait for clk_period; 
-  clk_rst_s <= '0'; 
-  wait; 
- end process;
+   begin
+   clk_rst_s <= '1'; 
+    wait for clk_period; 
+    clk_rst_s <= '0'; 
+    wait; 
+   end process;
 
 end;
