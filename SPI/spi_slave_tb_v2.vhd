a0d2512d22ac1ad1622d076d1f49cee9afc51a3b
amodica24 <anthony.modica23@gmail.com>
2019-05-04 17:30:30 -0700

    Update spi_slave_tb_v2.vhd

--- SPI/spi_slave_tb_v2.vhd
+++ SPI/spi_slave_tb_v2.vhd
@@ -9,17 +9,19 @@ architecture rtl of spi_slave_tb is
  component spi_slave is
   port (
      -- inputs
-     clk_in  : in  STD_LOGIC;    
-     d_in    : in  STD_LOGIC;    -- spi_slave serial d_in input
-     CS      : in  STD_LOGIC;    -- chip select input (active low)
-     d_out   : out STD_LOGIC_VECTOR (11 downto 0)
+     clk_in  : in  std_logic;    
+     d_in    : out std_logic;    -- spi_slave serial d_in input
+     CS      : in  std_logic;    -- chip select input (active low)
+     d_out   : in  std_logic;
+     data    : out std_logic_vector(9 downto 0)
    );
   end component spi_slave;
   
   signal clk_in_s     : std_logic           := '0';
   signal d_in_s       : std_logic;
   signal CS_s         : std_logic;
-  signal d_out_s      : std_logic_vector(11 downto 0);
+  signal d_out_s      : std_logic;
+  signal data_s       : std_logic_vector(9 downto 0);
 
 begin
   spi_slave_INST : spi_slave
@@ -27,35 +29,41 @@ begin
       clk_in  => clk_in_s,
       d_in    => d_in_s,
       CS      => CS_s,
-      d_out   => d_out_s
+      d_out   => d_out_s,
+      data    => data_s
       );
      clk_in_s <= not clk_in_s after 50 ns;
   
 process is
-  begin
-	wait for 50 ns;
-    CS_s <= '0';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '1';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '0';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '1';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '0';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '0';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '1';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '0';
-	wait until rising_edge(clk_in_s);
-	d_in_s <= '1';
-	wait until rising_edge(clk_in_s);
-	wait for 50 ns;
-	CS_s <= '1'; 
-	wait;
-     
+  begin 
+  -- d_in_s  <= '0'; 
+   d_out_s <= '0'; 
+   CS_s <= '1';
+   wait for 75 ns;
+   CS_s <= '0';
+   wait for 375 ns;
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '1';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '0';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '1';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '0';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '0';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '1';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '0';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '1';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '0';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '0';
+   wait until falling_edge(clk_in_s);
+   d_out_s <= '1';
+   wait;
   end process;
-   
 end rtl;
