e03dc60cba396930356179cdfae4cc047ae1a4ef
amodica24 <anthony.modica23@gmail.com>
2019-05-05 15:29:21 -0700

    Update clkDivide.vhd

--- clkDivide/clkDivide.vhd
+++ clkDivide/clkDivide.vhd
@@ -4,11 +4,11 @@
 -----------------------------------------------------------------------------
 -- File         :     clkDivide.vhd
 -- Description  :     This entity is a clock divider to be implemented
---                    with the analog-to-digital converter and multiplexer
+--                     with the analog-to-digital converter and multiplexer
 --
--- Inputs       :     clk_100MHz  - Input 100 MHz clock
---              :     clk_rst     - Reset for the clock
--- Outputs      :     clk_1MHz    - Output 1 MHz clock
+-- Inputs       :     clk_in  - Input for the clock
+--              :     clk_rst - Reset for the clock
+-- Outputs      :     new_clk - Output clock
 -----------------------------------------------------------------------------
 -- Version/Notes
 -- 1.0 - 2019-04-29 - Initial Version
@@ -21,10 +21,10 @@ use IEEE.numeric_std.ALL;
 
 entity clkDivide is
   port(
-    clk_100MHz  : in std_logic;
-    clk_rst     : in std_logic;
-    clk_1MHz    : out std_logic
-  );
+    clk_in  : in std_logic;
+    clk_rst : in std_logic; 
+	new_clk : out std_logic
+	);
 end clkDivide;
 
 architecture rtl of clkDivide is
@@ -32,19 +32,19 @@ architecture rtl of clkDivide is
   signal clk_count : integer := 0;
   signal div_value : integer := 100;
 begin
-  p1: process (clk_100MHz, clk_rst)
+  p1: process (clk_in, clk_rst)
     begin
       -- 100 MHz / 100 = 1 MHz 
       if (clk_rst = '1') then -- hold the clock during reset
        clk_count <= 0;
        temp_clk  <= '0';
-      elsif (rising_edge(clk_100MHz)) then
-       clk_count <= clk_count + 1;
-	     if (clk_count = div_value) then
-	      clk_count <= 0;
-	      temp_clk <= NOT temp_clk;
+      elsif (rising_edge(clk_in)) then
+	clk_count <= clk_count + 1;
+	if (clk_count = div_value) then
+	 clk_count <= 0;
+	 temp_clk <= NOT temp_clk;
+        end if;
        end if;
-      end if;
   end process;
- clk_1MHz <= temp_clk;
+ new_clk <= temp_clk;
 end rtl;
