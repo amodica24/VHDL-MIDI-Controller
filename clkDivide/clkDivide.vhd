d8cbf3a1178495bf07720555cc57bdf052e305b4
amodica24 <anthony.modica23@gmail.com>
2019-05-04 17:44:19 -0700

    Update clkDivide.vhd

--- clkDivide/clkDivide.vhd
+++ clkDivide/clkDivide.vhd
@@ -20,36 +20,31 @@ use IEEE.numeric_std.ALL;
 --Use work.resources.all;
 
 entity clkDivide is
-	port(
-		clk_in  : IN STD_LOGIC;
+  port(
+    clk_in  : IN STD_LOGIC;
 		clk_rst : IN STD_LOGIC; 
 		new_clk : OUT STD_LOGIC
 	);
 end clkDivide;
 
-architecture behav of clkDivide is
-	signal temp_clk : std_logic; 
+architecture rtl of clkDivide is
+  signal temp_clk  : std_logic; 
+  signal clk_count : integer := 0;
+  signal div_value : integer := 100;
 begin
-    p1: process (clk_in, clk_rst)
-	variable clk_count  : integer; -- keeps track of clock cycles
-	variable div_value  : integer; 
-	
-	
-	begin
-	-- 100 MHz / 100 = 1 MHz 
-	div_value := 100; -- divider for ADC sample rate of 62.5 ksps 
-	
-		if (clk_rst = '1') then -- hold the clock during reset
-            clk_count := 0;
-            temp_clk  <= '0';
-		elsif (clk_in'event AND clk_in = '1') then
-	       -- elsif(risingedge(clk_in)) then
-			clk_count := clk_count + 1;
-			if (clk_count = div_value) then
-				clk_count := 0;
-				temp_clk <= NOT temp_clk;
-			end if;
-		end if;	
-	end process;
-	new_clk <= temp_clk;
-end behav;
+  p1: process (clk_in, clk_rst)
+    begin
+      -- 100 MHz / 100 = 1 MHz 
+      if (clk_rst = '1') then -- hold the clock during reset
+       clk_count <= 0;
+       temp_clk  <= '0';
+      elsif (rising_edge(clk_in)) then
+	clk_count <= clk_count + 1;
+	if (clk_count = div_value) then
+	 clk_count <= 0;
+	 temp_clk <= NOT temp_clk;
+        end if;
+       end if;
+  end process;
+ new_clk <= temp_clk;
+end rlt;
